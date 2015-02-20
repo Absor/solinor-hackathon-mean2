a32ba4ef08dcbe020882390e55591c52555d857e
Jani Luukko <jj.luukko@gmail.com>
2015-02-21 00:57:55 +0200

    updates to crawler

--- crawler/apualol.js
+++ crawler/apualol.js
@@ -1,8 +1,35 @@
 var cr = require('./lulz.js');
+var fs = require('fs');
+var async = require('async');
 
-var haha = new cr("http://flowfestival.com/");
+var list = [];
 
-haha.crawl(function(res) {
-	console.log('go!');
-	console.log(res);
+var limit = 50;
+var current = 0;
+
+fs.readFile('../client/sites.txt', { encoding: 'utf8' }, function (err, data) {
+  if (err) throw err;
+  data = data.split('\n', limit);
+  /* data.forEach(function(url) {
+  	console.log(url);
+  	doSomething(url);
+  }); */
+
+  async.series(data.map(function(url) {
+  	return function(callback) {
+  		doSomething(url, function() {
+  			callback(null,null);
+  		});
+  	}
+  }));
 });
+
+function doSomething(url,cb) {
+	console.log("Going to: " + url);
+	var haha = new cr(url);
+
+	haha.crawl(function(res) {
+		console.log(res);
+		cb(res);
+	});
+}
