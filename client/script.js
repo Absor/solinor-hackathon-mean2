bf89e9ded5cf72c1c187395244e35320ec5d43bb
Jonne Airaksinen <jonne.airaksinen@gmail.com>
2015-02-20 21:41:12 +0200

    beginnings of the frontend

---
+++ client/script.js
@@ -0,0 +1,14 @@
+
+var blocks = [
+{
+	id: 1,
+	title: 'Google',
+	url: 'www.google.fi',
+	screenshot: '1-screenshot.png',
+	logo: '1-logo.png',
+	colors: ['#123456', '#AABBCC', '#FFDEAD'],
+},
+]
+
+$('');
+
